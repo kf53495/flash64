78d45747820d497fe296acace85af8acbb073909
kf53495 <fkmndk902902@gmail.com>
2022-05-04 22:20:35 +0900

    石を指定して色を帰られるように設定

--- memory64/static/memory.js
+++ memory64/static/memory.js
@@ -1,18 +1,9 @@
 const setStoneColor = () => {
-  const stoneColor = document.querySelectorAll('td');
+  let stoneColor = document.querySelectorAll('.circle');
   stoneColor.forEach(function(value) {
-    
-  })
+    console.log(value);
+    value.style.backgroundColor = "#ffffff"
+  });
 }
-
-// const setDate = () => {
-//   const date = new Date();
-//     document.getElementById("report_month").value = date.getMonth()+1;
-//     document.getElementById("report_date").value = date.getDate();
-//     document.getElementById("report_teacher").value = gon.teacher;
-
-//   const days = ["日", "月", "火", "水", "木", "金", "土"];
-//     document.getElementById("report_day").value = days[date.getDay()];
-// }
 
 window.addEventListener("load", setStoneColor)
