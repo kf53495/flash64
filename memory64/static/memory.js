a41b0d5d5d0c5dfe8a27eb770557e1e0d4ac0053
kf53495 <fkmndk902902@gmail.com>
2022-05-04 21:42:09 +0900

    全てのマスに黒石を配置

---
+++ memory64/static/memory.js
@@ -0,0 +1,18 @@
+const setStoneColor = () => {
+  const stoneColor = document.querySelectorAll('td');
+  stoneColor.forEach(function(value) {
+    
+  })
+}
+
+// const setDate = () => {
+//   const date = new Date();
+//     document.getElementById("report_month").value = date.getMonth()+1;
+//     document.getElementById("report_date").value = date.getDate();
+//     document.getElementById("report_teacher").value = gon.teacher;
+
+//   const days = ["日", "月", "火", "水", "木", "金", "土"];
+//     document.getElementById("report_day").value = days[date.getDay()];
+// }
+
+window.addEventListener("load", setStoneColor)
