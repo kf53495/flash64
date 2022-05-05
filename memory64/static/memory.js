5802c76596ca0f7334d3d408df6cd7958ac57639
kf53495 <fkmndk902902@gmail.com>
2022-05-05 14:02:35 +0900

    覚えたボタンをクリックすると盤面の石が消えるように設定

--- memory64/static/memory.js
+++ memory64/static/memory.js
@@ -6,6 +6,23 @@ const setStoneColor = () => {
       value.style.backgroundColor = "#ffffff"
     };
   });
-}
+};
 
-window.addEventListener("load", setStoneColor)
+const hideStones = () => {
+  const memorizedBtn = document.getElementById('btn')
+  const stoneColor = document.querySelectorAll('.circle');
+  function changeColor() {
+    stoneColor.forEach(function(value) {
+    value.style.backgroundColor = "#32cd32"
+    });
+  };
+  btn.addEventListener('click', changeColor);
+};
+// window.onload = function() {
+//   const memorizedBtn = document.querySelector('.memorized-btn')
+//   console.log(memorizedBtn)
+// }
+
+
+window.addEventListener('DOMContentLoaded', setStoneColor)
+window.addEventListener('DOMContentLoaded', hideStones)
