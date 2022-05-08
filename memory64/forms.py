cadb1699ace061bbceed1f0bfd87da9e4fa3ba94
kf53495 <fkmndk902902@gmail.com>
2022-05-08 18:15:59 +0900

    盤サイズを選択式に変更

--- memory64/forms.py
+++ memory64/forms.py
@@ -1,14 +1,13 @@
 from django import forms
 
 class SettingForm(forms.Form):
-    size = forms.CharField()
-    # size = forms.fields.ChoiceField(
-    #   choices = (
-    #     ('4', '4×4'),
-    #     ('6', '6×6'),
-    #     ('8', '8×8'),
-    #   ),
-    #   required=True,
-    #   widget=forms.widgets.Select
-    # )
+    size = forms.fields.ChoiceField(
+      choices = (
+        (4, '4×4'),
+        (6, '6×6'),
+        (8, '8×8'),
+      ),
+      required=True,
+      widget=forms.widgets.Select
+    )
     # time = forms.CharField() のちに実装予定
