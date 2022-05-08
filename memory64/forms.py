5eb7e4c3e83a6a61b396528744485e99e8f0a265
kf53495 <fkmndk902902@gmail.com>
2022-05-08 23:49:56 +0900

    盤サイズごとにcssを設定

--- memory64/forms.py
+++ memory64/forms.py
@@ -3,9 +3,9 @@ from django import forms
 class SettingForm(forms.Form):
     size = forms.fields.ChoiceField(
       choices = (
-        (4, '4×4'),
-        (6, '6×6'),
-        (8, '8×8'),
+        ('4', '4×4'),
+        ('6', '6×6'),
+        ('8', '8×8'),
       ),
       required=True,
       widget=forms.widgets.Select
