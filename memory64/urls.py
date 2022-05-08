00abb1337270251535c28d111a0f75a6fb1fb3c7
kf53495 <fkmndk902902@gmail.com>
2022-05-08 23:53:33 +0900

    MemoryViewの削除

--- memory64/urls.py
+++ memory64/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import IndexView, SelectionView, MemoryView #, TestView
+from .views import IndexView, SelectionView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
