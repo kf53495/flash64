085fc78c38b6959e5a47b92136596e9c05e7022e
kf53495 <fkmndk902902@gmail.com>
2022-05-10 19:25:43 +0900

    userアプリをsetting.pyに記載

--- config/settings.py
+++ config/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'memory64',
+    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'memory64',
 ]
 
 MIDDLEWARE = [
