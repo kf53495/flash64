4014f3bd9136d3969f90a62bc5b4e713c75a668c
kf53495 <fkmndk902902@gmail.com>
2022-05-12 18:51:37 +0900

    ログアウト機能を実装

--- config/settings.py
+++ config/settings.py
@@ -130,9 +130,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_URL = "/login/"
+LOGIN_URL = "/user/login/"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/login/"
+LOGOUT_REDIRECT_URL = "/user/login/"
 
 
 STATIC_URL = '/assets/'
