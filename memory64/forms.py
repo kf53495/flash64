8697e89b923b203ec4dcf1c19e5459eb86c5e72f
kf53495 <fkmndk902902@gmail.com>
2022-05-19 17:45:08 +0900

    formに5と7の選択肢を追加

--- memory64/forms.py
+++ memory64/forms.py
@@ -6,7 +6,9 @@ class SettingForm(forms.Form):
     size = forms.fields.ChoiceField(
       choices = (
         ('4', '4×4'),
+        ('5', '5×5'),
         ('6', '6×6'),
+        ('7', '7×7'),
         ('8', '8×8'),
       ),
       required=True,
